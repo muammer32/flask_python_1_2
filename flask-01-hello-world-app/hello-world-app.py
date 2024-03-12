a5993f0ad7191f01b90f8814894ac0811c87be1c
muammer <muammerhardal.gmail.com>
2024-03-12 11:29:04 +0300

    en yeni

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,36 +1,6 @@
 from flask import Flask 
 app = Flask(__name__)
 
-@app.route("/")
-def head():
-    return "hello word clarusway-17"
-
-@app.route("/second")
-def head2():
-    return "ahanda ikinci page"
-
-@app.route("/third")
-def head3():
-    return "buda 3 ncü sayfa"
-
-from flask import Flask 
-app = Flask(__name__)
-
-@app.route('/')
-def head():
-    return "hello word"
-
-@app.route('/second')
-def second():
-    return "This is second page"
-
-@app.route('/third')
-def third():
-    return "flask-01-02/This is third page"
-
-from flask import Flask 
-app = Flask(__name__)
-
 @app.route('/')
 def head():
     return "hello word"
@@ -47,8 +17,7 @@ def third():
 def forth(id):
     return f'Id of this page is {id}'
 
-
 if __name__ == '__main__':
 
-    # app.run(debug=True, port=30000)
-    app.run(host= '0.0.0.0', port=8081)
+    app.run(debug=True, port=30000)
+    # app.run(host= '0.0.0.0', port=8081)
